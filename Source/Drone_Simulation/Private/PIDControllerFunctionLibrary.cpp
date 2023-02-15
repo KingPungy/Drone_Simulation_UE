8ca3263cdbe7462db400c4193e5af11b1ddb75f4
KingPungy <scott.timmermans@gmail.com>
2023-02-15 16:11:43 +0100

    made screenshots and looked at code. no changes made  unreal is just being picky about binary files

--- Source/Drone_Simulation/Private/PIDControllerFunctionLibrary.cpp
+++ Source/Drone_Simulation/Private/PIDControllerFunctionLibrary.cpp
@@ -6,12 +6,13 @@
 float UPIDControllerFunctionLibrary::PIDCalculation(float DeltaTime, float P, float I, float D, float Bias, 
 	float CurrentValue, float DesiredValue, UPARAM(ref) float& Integral, UPARAM(ref) float& ErrorPrior)
 {	
-	float  pError = CurrentValue - DesiredValue;
+	float pError = CurrentValue - DesiredValue;
 	float pDerivative = (pError - ErrorPrior) / DeltaTime;
 	
-	//if (GEngine)GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, FString::Printf(TEXT("Time elapsed: %f seconds"), DeltaTime));
+	//if (GEngine)GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, 
+	//			FString::Printf(TEXT("Time elapsed: %f seconds"), DeltaTime));
 	
-	// Se referenced Variables
+	// Set referenced Variables
 	Integral += pError * DeltaTime;
 	ErrorPrior = pError;
 
@@ -22,7 +23,7 @@ float UPIDControllerFunctionLibrary::PIDCalculation(float DeltaTime, float P, fl
 float UPIDControllerFunctionLibrary::PIDCalculationRotation(float DeltaTime, float P, float I, float D, float Bias, 
 	float CurrentValue, float DesiredValue, UPARAM(ref) float& Integral, UPARAM(ref) float& ErrorPrior)
 {
-	float  pError = DesiredValue - CurrentValue;
+	float pError = DesiredValue - CurrentValue;
 
 	// Unreal Engine Rotation Error correction
 	if (pError > 180) {
@@ -35,7 +36,7 @@ float UPIDControllerFunctionLibrary::PIDCalculationRotation(float DeltaTime, flo
 
 	//if (GEngine)GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, FString::Printf(TEXT("Time elapsed: %f seconds"), DeltaTime));
 
-	// Se referenced Variables
+	// Set referenced Variables
 	Integral += pError * DeltaTime;
 	ErrorPrior = pError;
 
